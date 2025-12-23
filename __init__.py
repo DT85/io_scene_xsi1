9a96643aff6930ca7a016e8437004808ee5f28da
DT <tempust85@hotmail.com>
2025-12-23 10:55:47 +1000

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
 bl_info = {
-	"name": "Softimage XSI 1.0 format",
+	"name": "Softimage XSI 1.0 Export",
 	"author": "Tempust85",
 	"version": (1, 0, 0),
 	"blender": (4, 2, 0),
 	"location": "File > Import-Export",
-	"description": "Softimage XSI 1.0 Exporter. Based off of BZ2 XSI Exporter by frute94",
+	"description": "Export Softimage XSI 1.0. Based off of 'BZ2 XSI format' by frute94",
 	"category": "Import-Export"
 }
 
